8c18f4a37c13547db69f4fa79a3f72fa34278cdf
Loren <livelaughhodl@gmail.com>
2022-06-26 22:14:39 -0700

    Initial commit

---
+++ migrations/2_deploy_land.js
@@ -0,0 +1,10 @@
+const Land = artifacts.require("Land");
+
+module.exports = async function (deployer) {
+
+    const NAME = "Dapp U Buildings"
+    const SYMBOL = "DUB"
+    const COST = web3.utils.toWei('1', 'ether')
+
+  await deployer.deploy(Land, NAME, SYMBOL, COST);
+};
